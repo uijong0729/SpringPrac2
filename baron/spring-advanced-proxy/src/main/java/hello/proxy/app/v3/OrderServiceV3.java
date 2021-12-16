64a89ce6637dd4ef5236ffd747149d1cc37ec800
uijong0729 <krocha0729@gmail.com>
2021-12-16 23:44:19 +0900

    prac

---
+++ baron/spring-advanced-proxy/src/main/java/hello/proxy/app/v3/OrderServiceV3.java
@@ -0,0 +1,19 @@
+package hello.proxy.app.v3;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceV3 {
+	
+	private final OrderRepositoryV3 repo;
+	
+	public OrderServiceV3(OrderRepositoryV3 orderRepositoryV3) {
+		super();
+		this.repo = orderRepositoryV3;
+	}
+
+	public void orderItem(String itemId) {
+		// TODO Auto-generated method stub
+		repo.save(itemId);
+	}
+}
