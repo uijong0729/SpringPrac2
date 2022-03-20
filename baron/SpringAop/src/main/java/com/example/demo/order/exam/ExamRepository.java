ec35108bf844f312ad00739fa85eb6ff671b0ccc
uijong0729 <krocha0729@gmail.com>
2022-03-21 01:36:29 +0900

    apply aop

--- baron/SpringAop/src/main/java/com/example/demo/order/exam/ExamRepository.java
+++ baron/SpringAop/src/main/java/com/example/demo/order/exam/ExamRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.order.exam;
 
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.order.exam.annotation.Trace;
+
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -11,6 +13,7 @@ public class ExamRepository {
 	private static int seq = 0;
 	
 	// 5번에 한번 실패하는 요청
+	@Trace
 	public String save(String id) {
 		seq++;
 		log.info("seq={} (seq % 5 == 0)={}", seq, (seq % 5 == 0));
--- baron/SpringAop/src/main/java/com/example/demo/order/exam/ExamService.java
+++ baron/SpringAop/src/main/java/com/example/demo/order/exam/ExamService.java
@@ -2,6 +2,8 @@ package com.example.demo.order.exam;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.order.exam.annotation.Trace;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ExamService {
 	private final ExamRepository examRepository;
 	
+	@Trace
 	public void request(String id) {
 		log.info(examRepository.save(id));
 	}
